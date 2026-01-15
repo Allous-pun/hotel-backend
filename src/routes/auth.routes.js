ca0d37ab70bf2a03ca709b2c9f4f199e38b34a04
Aloyce CodeCraftsman <otienoaloyce9029@gmail.com>
2026-01-15 14:36:06 +0300

    Add UI

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,16 +4,20 @@ const {
   register, 
   login, 
   getProfile,
-  createStaffUser,
-  createAdminUser 
-} = require("../controllers/auth.controller"); 
+  createStaffUser,  // Make sure this is imported
+  createAdminUser   // Make sure this is imported
+} = require("../controllers/auth.controller");
 const authMiddleware = require("../middleware/authMiddleware");
-const { adminOnly } = require("../middleware/roleMiddleware"); 
+const { adminOnly } = require("../middleware/roleMiddleware");
 
+// Public routes
 router.post("/register", register);
 router.post("/login", login);
+
+// Protected routes
 router.get("/profile", authMiddleware, getProfile);
 
+// Admin-only routes for creating users
 router.post("/create-staff", authMiddleware, adminOnly, createStaffUser);
 router.post("/create-admin", authMiddleware, adminOnly, createAdminUser);
 
